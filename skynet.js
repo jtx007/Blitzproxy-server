bf6285bef29776ba693b90b4438ec17a822526b7
jtx007 <jamesjacobthomas7@gmail.com>
2019-01-15 00:07:54 -0500

    proxy server test with sample api

--- skynet.js
+++ skynet.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello James!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))  
+
+const request = require('request')
+request('https://ghibliapi.herokuapp.com', function(error, response, body){
+    console.log('error:' error);//Print error if applicable
+    console.log('statuscode:', response && response.statusCode); //Print the response status code if a response was received
+    console.log('body:', body);
+
+})
