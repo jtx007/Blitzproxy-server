90b8471f206bebf2d1763b89432188301d4d3e41
jtx007 <jamesjacobthomas7@gmail.com>
2019-01-15 00:56:09 -0500

    added cors middleware

--- skynet.js
+++ skynet.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3000
 
+app.use(cors());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
