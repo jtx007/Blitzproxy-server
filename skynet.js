78e570eb05d0cac711bbe9e5fe536d920719b787
jtx007 <jamesjacobthomas7@gmail.com>
2019-01-15 00:42:51 -0500

    got sample api response to console log and in local host port

--- skynet.js
+++ skynet.js
@@ -3,13 +3,14 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req, res) => res.send('Hello James!'))
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 const request = require('request')
 request('https://ghibliapi.herokuapp.com/films', function(error, response, body){
     console.log('error:', error);//Print error if applicable
     console.log('statuscode:', response && response.statusCode); //Print the response status code if a response was received
     console.log('body:', body);
+    app.get('/', (req, res) => res.send(body))
+
 
 })
