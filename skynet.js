c80a0d64d536869deff51c05ed7435e85416f145
jtx007 <jamesjacobthomas7@gmail.com>
2019-02-03 01:46:40 -0500

    MVP build

--- skynet.js
+++ skynet.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const request = require('request')
 
 let latLngCoords
