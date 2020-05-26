27d3873557f17a2e3f1151d8977aed83b5f37e0b
jtx007 <jamesjacobthomas7@gmail.com>
2020-05-26 04:51:39 -0400

    trying cors fix

--- skynet.js
+++ skynet.js
@@ -6,7 +6,45 @@ const port = process.env.PORT || 3000
 const request = require('request')
 
 let latLngCoords
-app.use(cors());
+conf = {
+  // look for PORT environment variable,
+  // else look for CLI argument,
+  // else use hard coded value for port 8080
+  port: process.env.PORT || process.argv[2] || 8080,
+
+  // origin undefined handler
+  // see https://github.com/expressjs/cors/issues/71
+  originUndefined: function (req, res, next) {
+    if (!req.headers.origin) {
+      res.json({
+        mess:
+          "Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined",
+      });
+    } else {
+      next();
+    }
+  },
+
+  // Cross Origin Resource Sharing Options
+  cors: {
+    // origin handler
+    origin: function (origin, cb) {
+      // setup a white list
+      let wl = ["https://blitzweatherapp.herokuapp.com/"];
+
+      if (wl.indexOf(origin) != -1) {
+        cb(null, true);
+      } else {
+        cb(new Error("invalid origin: " + origin), false);
+      }
+    },
+
+    optionsSuccessStatus: 200,
+  },
+};
+
+app.use(conf.originUndefined, cors(conf.cors));
+
 
 app.listen(port, () => console.log('Weather app spinning up, send request from frontend'))
 
