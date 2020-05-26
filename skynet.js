41a699a05b9a4166b404ed51825aa1fa4a808c01
jtx007 <jamesjacobthomas7@gmail.com>
2020-05-26 05:09:13 -0400

    updated cors link

--- skynet.js
+++ skynet.js
@@ -9,7 +9,7 @@ let latLngCoords
 
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
